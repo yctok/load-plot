05fd354221dbb1b36fd05f932e940425af79ccca
yctok <ychuang@wm.edu>
2022-10-01 00:13:14 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -7,16 +7,16 @@ This is a temporary script file.
 import numpy as np
 import matplotlib.pyplot as plt
 
-exp_file = np.genfromtxt('yag_27205_275.dat',delimiter=' ')
+
+exp_file = np.genfromtxt('C:/Users/user/Documents/SOLPS data/simulation data/mast/yag_27205_275.dat',delimiter=' ')
 
 psi_n = exp_file[:,0]
 plt.plot(exp_file[:,0], exp_file[:,2])
 plt.show()
 
-data_file = np.genfromtxt('ne3da.txt',delimiter=',')
+data_file = np.genfromtxt('C:/Users/user/Documents/SOLPS data/simulation data/mast/ne3da.last10',delimiter='\t')
 
 R_Rsep = data_file[:,0]
 plt.plot(data_file[:,0], data_file[:,1])
 plt.show()
-
 
