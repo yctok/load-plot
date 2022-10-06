441efe818393550321d907d69b18bc446253fbd4
yctok <ychuang@wm.edu>
2022-10-06 12:21:12 -0400

    Update modify_equ.py

--- modify_equ.py
+++ modify_equ.py
@@ -47,3 +47,29 @@ gwrite = re.sub('[-+]?\d+\.?\d+[eE]?[-+]\d+', '3.12345678e-03', string2)
 print(gwrite)
 
 """
+file ='C:/Users/user/Documents/SOLPS data/simulation data/mast/g027205.00275.X2.equ'
+
+with open(file) as f:
+     datalist =f.readlines()
+
+j= 17
+
+while j< 43:
+    string2 = datalist[j]
+    gdata = re.findall('[-+]?\d+\.?\d+[eE]?[-+]\d+', string2)
+    print(gdata)
+    mylist =[]
+    for ii in gdata:
+        a = np.float64(ii) +np.float64(1.00000000)
+        b = "{:.8E}".format(a)
+        mylist.append(b)
+    print(mylist)
+    writelist = ' '.join(str(x)+"   " for x in mylist)
+    datalist[j] = "\t" + writelist + "\n"
+    print(datalist[j])
+    j= j+ 1
+
+with open(file,'w') as g:
+    for i,line in enumerate(datalist,0):         ## STARTS THE NUMBERING FROM 1 (by default it begins with 0)    
+        g.writelines(line)
+
