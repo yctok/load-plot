0d98530b84fb638fee5577b0b232cf392155b992
yctok <ychuang@wm.edu>
2023-04-03 18:36:58 -0400

    Create cood_dr.py

---
+++ poster plot generator/cood_dr.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  3 16:31:40 2023
+
+@author: Yi-Cheng
+"""
+
+
+
+
+xp = sxp.SOLPSxport(workdir=os.getcwd(), gfile_loc=gfile_loc, impurity_list=impurity_list)
