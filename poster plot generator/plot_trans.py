57fad99950e1863a4f1362e93bf028fba248298a
yctok <ychuang@wm.edu>
2023-04-18 17:59:45 -0400

    Update plot_trans.py

--- poster plot generator/plot_trans.py
+++ poster plot generator/plot_trans.py
@@ -14,14 +14,16 @@ import glob
 dev = 'mast'
 shot = '027205'
 shift = 'org'
-series = ['try & error','p4_d6', 'p4_d6_11',
+series = ['try & error','p4_d6_11', 'p4_d6_11',
           '1ex_input0.1%_ts6_series', '4ex_n_decay0.06_ts6_series',
-          '5ex_n_decay0.03_ts6_series', '6ex_input0.2%_ts6_series']
+          '5ex_n_decay0.03_ts6_series', '6ex_input0.2%_ts6_series',
+          'p4_d6_7', 'no_flux_false']
 ex1 = '1ex_p4_input0.1%_'
 ex4 = '4ex_p4_d6'
 ex5 = '5ex_p4_d3'
 ex6 = '6ex_p4_input0.2%_'
-s_choose = 5
+ex7 = '7ex_p4_d6'
+s_choose = 1
 
 basedrt, topdrt = tl.set_wdir()
 
@@ -73,6 +75,22 @@ elif s_choose == 5:
     for ii in range(len(b_list)):
         a_list.append(b_list[ii])
 
+elif s_choose == 6:
+    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[7]))
+    b_list = glob.glob('{}/{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[8], ex7))
+    print(b_list)
+    a_list.sort(key=tl.a_number)
+    b_list.sort(key=tl.ex1_number)
+
+    for ii in range(len(b_list)):
+        a_list.append(b_list[ii])
+        
+elif s_choose == 7:
+    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[2]))
+    # a_list = glob.glob('{}/{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[8], ex7))
+    a_list.sort(key=tl.a_number)
+    # a_list.sort(key=tl.ex1_number)
+
 
 n_dir = len(a_list)
 
@@ -99,7 +117,6 @@ for k in range(n_dir):
     
    
 # psi = tl.dsa_to_psi(trans_dic[attempt_name[0]]['1'][0,:])
-  
 # plt.plot(trans_dic[attempt_name[0]]['1'][0,:], trans_dic[attempt_name[0]]['1'][1,:], label= attempt_name[0])
 # plt.plot(psi, trans_dic[attempt_name[0]]['1'][1,:], label= attempt_name[0])
 
