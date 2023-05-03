efd6059257ae79b16e3eb82bc90bf79a4f0f0095
yctok <ychuang@wm.edu>
2023-05-03 11:20:10 -0400

    Update tailor_trans_coef.py

--- poster plot generator/tailor_trans_coef.py
+++ poster plot generator/tailor_trans_coef.py
@@ -14,8 +14,8 @@ import glob
 dev = 'mast'
 shot = '027205'
 shift = 'org_new_series'
-series = ['hc7dn5_6_nts5_a']
-
+series = ['h7d5orgb_del0.001_15_nts5_a']
+n = '16'
 s_choose = 0
 
 basedrt, topdrt = tl.set_wdir()
@@ -37,42 +37,42 @@ m = len(yd)
 mod_y = np.zeros(m)
 for j in range(m):
     if j< 13:
-        mod_y[j] = 10.00
-    elif j>= 13 and j <= 21:
+        mod_y[j] = 22
+    elif j>= 13 and j <= 18:
         mod_y[j] = cod[j,1]
     
     else:
-        mod_y[j] = 3.00
+        mod_y[j] = 2.42
         
 cod[:,1] = mod_y
 
 mod_yki = np.zeros(m)
 for j in range(m):
-    if j< 15:
-        mod_yki[j] = 5.50
-    elif j>= 15 and j <= 16:
+    if j< 13:
+        mod_yki[j] = 6.00
+    elif j>= 13 and j <= 17:
         mod_yki[j] = coki[j,1]
     
     else:
-        mod_yki[j] = 4.5
+        mod_yki[j] = 2.60
         
 coki[:,1] = mod_yki
 
-mod_yke = np.zeros(m)
-for j in range(m):
-    if j< 14:
-        mod_yke[j] = 7.00
-    elif j>= 14 and j <= 16:
-        mod_yke[j] = coke[j,1]
+# mod_yke = np.zeros(m)
+# for j in range(m):
+#     if j< 14:
+#         mod_yke[j] = 7.00
+#     elif j>= 14 and j <= 17:
+#         mod_yke[j] = coke[j,1]
     
-    else:
-        mod_yke[j] = 4.50
+#     else:
+#         mod_yke[j] = 4.63
         
-coke[:,1] = mod_yke
+# coke[:,1] = mod_yke
 
 
 b = b2tp.Generate(cod, CoeffID=1, SpeciesID=2, M=[1])
-c = b2tp.WriteInputfile(file='b2.transport.inputfile', points= trans_list ,M_1 = True, M=[1])
+c = b2tp.WriteInputfile(file='b2.transport.inputfile_{}'.format(n), points= trans_list ,M_1 = True, M=[1])
 
 
 
