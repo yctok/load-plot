ca6088cd40a53c5c910f705ca9b16be6249f80b4
yctok <ychuang@wm.edu>
2023-05-03 11:20:12 -0400

    Update plot_trans.py

--- poster plot generator/plot_trans.py
+++ poster plot generator/plot_trans.py
@@ -13,92 +13,39 @@ import glob
 
 dev = 'mast'
 shot = '027205'
-shift = 'org'
-series = ['try & error','p4_d6_11', 'p4_d6_11',
-          '1ex_input0.1%_ts6_series', '4ex_n_decay0.06_ts6_series',
-          '5ex_n_decay0.03_ts6_series', '6ex_input0.2%_ts6_series',
-          'p4_d6_7', 'no_flux_false']
-ex1 = '1ex_p4_input0.1%_'
-ex4 = '4ex_p4_d6'
-ex5 = '5ex_p4_d3'
-ex6 = '6ex_p4_input0.2%_'
-ex7 = '7ex_p4_d6'
+shift_a = 'one_LS'
+series_a = ['lsts5_tw_17_one_a']
+shift_b = 'org_new_series'
+series_b = ['24l_tw_27_nts5_a']
+
 s_choose = 1
 
 basedrt, topdrt = tl.set_wdir()
 
 if s_choose == 0:
-    a_list = glob.glob('{}/{}/{}/{}/{}/p4_*'.format(basedrt, dev, shot, shift, series[0]))
-    a_list.sort(key=tl.a_number)
-    
-    
+    a_list = glob.glob('{}/{}/{}/{}/{}/'.format(basedrt, dev, shot, shift_a, series_a[0]))
+    a_list.sort(key=tl.new_number)
 elif s_choose == 1:
-    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[1]))
-    a_list.sort(key=tl.a_number)
-    
-elif s_choose == 2:
-    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[2]))
-    b_list = glob.glob('{}/{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[3], ex1))
-    a_list.sort(key=tl.a_number)
-    b_list.sort(key=tl.ex1_number)
+    a_list = glob.glob('{}/{}/{}/{}/{}/'.format(basedrt, dev, shot, shift_b, series_b[0]))
+    a_list.sort(key=tl.new_number)
+      
+key = tl.new_number(a_list[0])
+print(key)
 
-    for ii in range(len(b_list)):
-        a_list.append(b_list[ii])
-
-elif s_choose == 3:
-    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[2]))
-    b_list = glob.glob('{}/{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[4], ex4))
-    print(b_list)
-    a_list.sort(key=tl.a_number)
-    b_list.sort(key=tl.ex1_number)
-
-    for ii in range(len(b_list)):
-        a_list.append(b_list[ii])
-
-elif s_choose == 4:
-    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[2]))
-    b_list = glob.glob('{}/{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[5], ex5))
-    print(b_list)
-    a_list.sort(key=tl.a_number)
-    b_list.sort(key=tl.ex1_number)
-
-    for ii in range(len(b_list)):
-        a_list.append(b_list[ii])
-        
-elif s_choose == 5:
-    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[2]))
-    b_list = glob.glob('{}/{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[6], ex6))
-    print(b_list)
-    a_list.sort(key=tl.a_number)
-    b_list.sort(key=tl.ex1_number)
-
-    for ii in range(len(b_list)):
-        a_list.append(b_list[ii])
-
-elif s_choose == 6:
-    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[7]))
-    b_list = glob.glob('{}/{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[8], ex7))
-    print(b_list)
-    a_list.sort(key=tl.a_number)
-    b_list.sort(key=tl.ex1_number)
-
-    for ii in range(len(b_list)):
-        a_list.append(b_list[ii])
-        
-elif s_choose == 7:
-    a_list = glob.glob('{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[2]))
-    # a_list = glob.glob('{}/{}/{}/{}/{}/{}*'.format(basedrt, dev, shot, shift, series[8], ex7))
-    a_list.sort(key=tl.a_number)
-    # a_list.sort(key=tl.ex1_number)
+# name = a_list[0]
+# t = name.split("/",-1)[-2]
+# print(t)
 
 
 n_dir = len(a_list)
 
 attempt_name = []
 
+
 for j in range(n_dir):
     name = a_list[j]
-    attempt_name.append(name.split('\\',1)[1])
+    attempt_name.append(name.split("/",-1)[-2])
+
 
 file='b2.transport.inputfile'
 fd_list = []
@@ -108,7 +55,8 @@ for i in range(n_dir):
 
 
 trans_dic = {}
-trans_list = ['1', '3', '4']
+# trans_list = ['1','3', '4']
+trans_list = ['1', '4']
 
 for k in range(n_dir):
     trans_dic[attempt_name[k]] = b2tp.InputfileParser(fd_list[k], plot= False)
@@ -128,13 +76,16 @@ nn = 0
 
 
 for d in trans_list:
-    plt.figure(nn)
-    plt.yscale('log')
+    # plt.figure(nn)
+    # plt.yscale('log')
     for e in range(n_dir):
-        plt.plot(trans_dic[attempt_name[e]][d][0,:], trans_dic[attempt_name[e]][d][1,:], label= attempt_name[e])
-    plt.xlabel('radial coordinate: ', fontdict={"family":"Times New Roman","size": 20})
-    plt.ylabel(plot_dic[d][1], fontdict={"family":"Times New Roman","size": 20})
-    plt.title(plot_dic[d][0],fontdict={"family":"Times New Roman","size": 20})
+        plt.rcParams.update({'font.size': 20})
+        # plt.plot(trans_dic[attempt_name[e]][d][0,:], trans_dic[attempt_name[e]][d][1,:], label= attempt_name[e])
+        plt.plot(trans_dic[attempt_name[e]][d][0,:], trans_dic[attempt_name[e]][d][1,:],'o-', label= plot_dic[d][0])
+    plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Calibri","size": 20})
+    plt.ylabel(plot_dic[d][1], fontdict={"family":"Calibri","size": 20})
+    # plt.title(plot_dic[d][0],fontdict={"family":"Times New Roman","size": 20})
+    plt.title('Transport coefficient',fontdict={"family":"Calibri","size": 20})
     plt.legend()
     nn = nn + 1
 
