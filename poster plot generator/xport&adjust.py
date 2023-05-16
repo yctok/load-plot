65cbe434168d9f0ad7f6cf2adcfdf86c2fc732c1
yctok <ychuang@wm.edu>
2023-05-16 15:24:58 -0400

    Update xport&adjust.py

--- poster plot generator/xport&adjust.py
+++ poster plot generator/xport&adjust.py
@@ -15,7 +15,7 @@ import glob
 dev = 'mast'
 shot = '027205'
 shift = 'org_new_series'
-series = ['orgb_tw_17_nts5_a']
+series = ['ch_hh_30_nts5_a']
 n = '20tw'
 s_choose = 0
 
@@ -28,7 +28,7 @@ if s_choose == 0:
 xport_list = b2tp.InputfileParser(a_list[0], plot= False)
 
 
-trans_list = b2tp.InputfileParser('b2.transport.inputfile_18tw', plot= False)
+trans_list = b2tp.InputfileParser('C:/Users/user/Documents/GitHub/transport_inputfile/b2.transport.inputfile_29hh', plot= False)
 cod = trans_list['1'].T
 coki = trans_list['3'].T
 coke = trans_list['4'].T
@@ -50,9 +50,10 @@ for k in specieslist:
         plt.figure(i + 1)
         if k == '1':
             plt.axvline(x=trans_list[k][0,12], color='black',lw=3)
-            plt.axvline(x=trans_list[k][0,19], color='black',lw=3)
+            plt.axvline(x=trans_list[k][0,25], color='black',lw=3)
         if k == '4':
-            plt.axvline(x=trans_list[k][0,13], color='black',lw=3)
+            plt.yscale('log')
+            plt.axvline(x=trans_list[k][0,12], color='black',lw=3)
             plt.axvline(x=trans_list[k][0,17], color='black',lw=3)
         plt.plot(trans_list[k][0,:], trans_list[k][1,:],'o-', color = 'green', label ='adjust transport coefficient')
         plt.plot(xport_list[k][0,:], xport_list[k][1,:],'o-', color = 'orange', label ='xport code generate transport coefficient')
